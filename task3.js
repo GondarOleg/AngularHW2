00f6d21898059902647f90ea53303bfa32cf50f0
Гондарь Олег Николаевич <oleh.hondar@a-bank.com.ua>
2018-04-26 15:50:36 +0300

    3 задачка

--- task3.js
+++ task3.js
@@ -1,1 +1,21 @@
 
+function checkImage(pathToFile) {
+    var img = new Image();
+    return new Promise((resolve, reject) => {
+        img.onload = () => {
+            resolve("Файл есть!");
+        };
+        img.onerror = () => {
+            reject("Файла нет!");
+        };
+        img.src = pathToFile;
+    });
+}
+
+function checkImages(imagesPaths) {
+    for (imagePath of imagesPaths){
+        checkImage(imagePath).then(msg => alert(msg), error => alert(error));;
+    }
+}
+
+checkImages(["path1", "path2", "path3"]);
