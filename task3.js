6614d5dfca7e6a0d0adf7c2d47ab3cb5ffa96770
Гондарь Олег Николаевич <oleh.hondar@a-bank.com.ua>
2018-04-26 16:10:40 +0300

    3 задачка

--- task3.js
+++ task3.js
@@ -3,13 +3,13 @@ function checkImage(pathToFile) {
     var img = new Image();
     return new Promise((resolve, reject) => {
         img.onload = () => {
-            resolve("Файл есть!");
-        };
-        img.onerror = () => {
-            reject("Файла нет!");
-        };
-        img.src = pathToFile;
-    });
+        resolve("Файл есть!");
+    };
+    img.onerror = () => {
+        reject("Файла нет!");
+    };
+    img.src = pathToFile;
+});
 }
 
 function checkImages(imagesPaths) {
@@ -17,5 +17,10 @@ function checkImages(imagesPaths) {
         checkImage(imagePath).then(msg => alert(msg), error => alert(error));;
     }
 }
+
+/** С сайта https://github.com/GondarOleg/AngularHW2,
+ если открыть консоль, положительный кейс работает с таким массивом */
+
+checkImages(["https://assets-cdn.github.com/images/spinners/octocat-spinner-32.gif"]);
 
 checkImages(["path1", "path2", "path3"]);
